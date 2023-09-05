24b2b10ceacf858c15b656ecb5a8e724bc8b12ed
pravesh012 <chhetripravesh2@gmail.com>
2023-09-05 16:33:00 -0400

    changes

--- app/components/maincomponent.tsx
+++ app/components/maincomponent.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { FontAwesomeIcon } from "@/node_modules/@fortawesome/react-fontawesome/index";
-import { faSearch, faXmark } from "@/node_modules/@fortawesome/free-solid-svg-icons/index";
+import { faSearch, faXmark, faCaretDown } from "@/node_modules/@fortawesome/free-solid-svg-icons/index";
 import { GetData } from "../fetch_data_schemas/get_lang";
 import { JSXElementConstructor, ReactElement, ReactNode, useEffect, useState } from "react";
 import { languageClicked } from "./languageClicked";
+
 
 // defining few other client side component here cause i have the fectch data here//
 
@@ -11,7 +12,11 @@ import { languageClicked } from "./languageClicked";
 export function Main(){
     const [data , setdata ] = useState(undefined);
     const [ options , setoptions ] = useState({ display: 'none'
-     })
+     });
+     const [ typedSearch, setTypedSerach ] = useState([]);
+     const [ hideSpans , setHideSpans ] = useState('block');
+
+
 
     useEffect(()=>{
 
@@ -24,6 +29,46 @@ export function Main(){
     }, []);
 
 
+
+    // useEffect(()=>{
+
+    //     const StringValue = typedSearch[ typedSearch.length - 1];
+    //     console.log(data)
+    //     if(data){
+
+    //         data.filter(( _element: any , _index: number ) =>{
+    //             const options = 
+    //         })
+
+    //     }
+    //     else{
+    //         console.error('error data is empty')
+    //     }
+
+
+    // }, [typedSearch])
+
+
+    const SearchEngine = ( event: any )=>{        
+        const eventvalue = event.target.value;
+            eventvalue.length > 1 ? setHideSpans('none') : setHideSpans('block')
+
+            console.log(hideSpans)
+        if(eventvalue == ''){
+           setTypedSerach([]); 
+        }
+        else{
+            const result = [...typedSearch, eventvalue];
+            setTypedSerach(result);
+        }
+        
+
+
+            
+
+
+    }
+
     return(
         <>
         <main>
@@ -31,25 +76,26 @@ export function Main(){
                 {/* this below div is the languages */}
                 <div style = {options} className = " language-div"  >
                     <span className="inputsearch-container">
-                        <input placeholder = ' Translate from' className = 'search-bar-input' type= 'text' ></input>
+                        <input placeholder = ' Translate from' className = 'search-bar-input' type= 'text' onChange= {SearchEngine} ></input>
                         <span className="search-icons"> <FontAwesomeIcon  icon = {faSearch} /> </span>
                     </span>
                     <span className="fontawesomeicon-container-search">
                         <span onClick = {()=>{ setoptions({display: 'none'}) }} className="faXMARK"><FontAwesomeIcon icon = {faXmark} /></span>
                     </span>
 
-
-                    <div className = 'GET-LANGUAGE-CONTAINER' onClick = { languageClicked } >
+                        {/* hide the parent instead of child, child is grid */}
+                    <div className = 'parent-GET-CONTAINER' style = {{ display:hideSpans }}>
+                        <div className = 'GET-LANGUAGE-CONTAINER' onClick = { languageClicked } >
                         {/* i cannot */}
-                        { ( () =>{
-                            let values: any[] = []
-                            data && data.forEach ((element: any, index: number ) =>{
-                                return values.push(<span key = { index }>{element.name}</span>);
-                            })
-                            return values;
-                        })()}
+                        { 
+                         data && data.map ((element: any, index: number ) =>{
+                            return <span  key = { index }>{element.name}</span>;
+                        })
+                        }
                         
                     </div>
+                    </div>
+                    
                 </div> 
                
 
@@ -59,9 +105,10 @@ export function Main(){
                     {/* children are the main content parent as used as conatiner */}
                         {/* disabled disbable the arrtribute */}
                         
-                        <select onClick = {()=>{ setoptions({display: 'block'}) }}className = ' select-lang-1'>
+                        <div onClick = {()=>{ setoptions({display: 'block'}) }} className = ' select-lang-1'>English
+                            <FontAwesomeIcon className = 'dropdown-div'icon = { faCaretDown }/>
 
-                        </select>
+                        </div>
                         <textarea name = 'value' className= "input-left-translate"  placeholder = 'Enter Text'></textarea>
 
 
